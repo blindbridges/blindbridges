87f508346f92667d741b2d7260d42154e4b25320
brogar2000 <garrettsworld2000@gmail.com>
2021-01-23 23:01:53 +0000

    Added link to our Git Hub page, as well as a link to the licence.

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <h1 style="position: fixed; bottom: 0; width:100%; text-align: center">Footer</h1>
-<p style="position: fixed; bottom: 0; width:100%; text-align: center">Copyright© 2020-2021, Blind Bridges. All rights reserved.</p>
-<p style="position: fixed; bottom: 0; width:100%; text-align: center">This website was last updated on January 22, 2021 at 21:18 Central US time.</p>
+<p style="position: fixed; bottom: 0; width:100%; text-align: center">Copyright&copy; 2020-2021, Blind Bridges. [This website is open soursed, and is licensed under the <a href="https://www.gnu.org/licenses/lgpl-3.0.html">GPL3 License</a>. <a href="https://github.com/blindbridges/blindbridges/">Click here to visit the website's Git Hub page.</a></p>
+<p style="position: fixed; bottom: 0; width:100%; text-align: center">This website was last updated on January 23, 2021 at 17:00 Central US time.</p>
