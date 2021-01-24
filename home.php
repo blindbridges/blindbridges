ec78d7204133ae050ecae14dbb8fd6e5f50cce84
brogar2000 <garrettsworld2000@gmail.com>
2021-01-24 06:21:56 +0000

    Fixed quite a few spelling errors.

--- home.php
+++ home.php
@@ -4,12 +4,12 @@
 <p>A few major changes have been made.</p>
 <ul>
 <li>Introducing short episodes! These episodes are exactly 5 minutes long and are perfect for listening when you don't have time to listen to a full episode. You can listen to our first short <a href="podcast/short1">here</a>
-<li>Added headings to the podcast page to distinguish full episodes from short eppisodes
+<li>Added headings to the podcast page to distinguish full episodes from short episodes
 <li>We now have a schedule for uploading podcasts. We will upload regular podcasts every other Monday, but we'll also upload short episodes from time to time on Saturday as a bonus.
-<li>This website is now open soursed! For those who aren't sure what that means, the viewers of this website can now <a href="https://github.com/blindbridges/blindbridges">directly contribute</a> by adding features and pages, as well as fixing issues. Don't worry; every contribution has to be reviewed before it makes it onto the website itself, so nothing can be tampered with or destroied.
+<li>This website is now open sourced! For those who aren't sure what that means, the viewers of this website can now <a href="https://github.com/blindbridges/blindbridges">directly contribute</a> by adding features and pages, as well as fixing issues. Don't worry; every contribution has to be reviewed before it makes it onto the website itself, so nothing can be tampered with or destroyed.
 <h3>January 22, 2021</h3>
 <p>We made a miner change to the episode page. You can now see the duration of the podcast, as well as the date it was uploaded.</p>
 <h3>January 19, 2021</h3>
-<p>Our first podcast "<a href="podcast/ep1">Changing a Wall Outlet</a>" has been uploaded, and we now have an <a href="podcast/podcast.xml">RSS fead</a>! Miner site appearance changes have been made.</p>
+<p>Our first podcast "<a href="podcast/ep1">Changing a Wall Outlet</a>" has been uploaded, and we now have an <a href="podcast/podcast.xml">RSS feed</a>! Miner site appearance changes have been made.</p>
 <h3>January 11, 2021</h3>
 <p>The website is up and in working order. More pages will be added over time.</p>
