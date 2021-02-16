7faba28ca4a6b94da2fb28f6a3c25a6396979d2e
brogar2000 <garrettsworld2000@gmail.com>
2021-02-16 05:07:04 +0000

    Added page for episode 2 of the podcast.

---
+++ podcast/ep2/ep2.php
@@ -0,0 +1,33 @@
+<h3>Episode 2, The Joys of Moving</h3>
+<ul>
+<li>Duration: 15 Minutes
+<li>Date: February 15, 2021
+</ul>
+<p>In this episode, Firecat shows you how to pack boxes, as well as other things related to moving to a new home.</p>
+<p>You can play the episode below, or you can <a href="ep2.mp3">click here to download it</a>.</p>
+<button class="accordion">Audio Player</button>
+<div class="panel">
+<audio controls>
+<source src = "ep2.mp3" type = "audio/mpeg">
+      </audio>
+</div>
+<h4>Comments</h4>
+<div id="disqus_thread"></div>
+<script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = http://blindbridges.org/podcast/ep2;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = podep2; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://blindbridges.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
